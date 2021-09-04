367350ba0f7db30a958e45a4caa7c6cd54ac61db
biaaimi <biagioni.aimi@gmail.com>
2021-09-03 23:16:40 -0300

    nome completo

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace exercicios_etec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.Write(" Digite seu primeiro nome:");
+
+            string primeironome= Console.ReadLine();
+            
+            Console.ResetColor();
+            Console.WriteLine();
+
+            string sobrenome= Console.ReadLine();
+
+            Console.ForegroundColor= ConsoleColor.DarkGreen;
+            Console.WriteLine();
+
+            Console.ForegroundColor= ConsoleColor.DarkYellow;
+            Console.Write("Nome completo: ");
+            Console.WriteLine($"{primeironome} {sobrenome}");
+
+            Console.ForegroundColor= ConsoleColor.DarkMagenta;
+            Console.Write("Nome de catálogo: ");
+            Console.Write(sobrenome.ToUpper());
+            Console.Write(" , ");
+            Console.WriteLine(primeironome);
+
+            Console.ResetColor();
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
+
